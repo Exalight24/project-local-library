0fc51e5835671f9ea0b4426982d4f791e5c9f049
brandon neer <btnphotography@gmail.com>
2020-12-16 12:34:07 -0600

    first commit

--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,34 @@
-function findAuthorById(authors, id) {}
+function findAuthorById(authors, id) {
+  return authors.find((author)=>author.id === id )
+}
 
-function findBookById(books, id) {}
+function findBookById(books, id) {
+  return books.find((book)=>book.id === id )
+}
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+  let falseArray = books.filter(book=>{
+   return book.borrows[0].returned==false
+  });
+  let trueArray = books.filter(book=>{
+    return book.borrows[0].returned==true
+  });
+  return [falseArray, trueArray];
+}
 
-function getBorrowersForBook(book, accounts) {}
+function getBorrowersForBook(book, accounts) {
+  let borrowers = [];
+  accounts.forEach(account=>{
+    book.borrows.forEach(transaction=>{
+      if(transaction.id === account.id){
+        let accountObj = {...account};
+        accountObj.returned=transaction.returned;
+        borrowers.push(accountObj)
+      }
+    })
+  })
+return borrowers.slice(0,10)
+}
 
 module.exports = {
   findAuthorById,
